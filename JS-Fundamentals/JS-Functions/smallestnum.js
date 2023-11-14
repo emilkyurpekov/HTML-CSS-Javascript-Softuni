2f09fcb9f84654e2615fb15672a01a01c45911fb
Emil Kyurpekov <64215369+emilkyurpekov@users.noreply.github.com>
2023-11-14 17:41:08 +0200

    Add files via upload

---
+++ JS-Fundamentals/JS-Functions/smallestnum.js
@@ -0,0 +1,4 @@
+function smallestnumber(first,second,third){
+    console.log(Math.min(first,second,third));
+}
+smallestnumber(3,2,-10)
---
+++ JS-Fundamentals/JS-Functions/sumTwoNums.js
@@ -0,0 +1,9 @@
+function sum(one,two,three){
+   let sum = (one, two) =>{
+        return one + two;
+   }
+   function substract(){
+        console.log(sum- three);
+   }
+}
+sum(23,6,10);
